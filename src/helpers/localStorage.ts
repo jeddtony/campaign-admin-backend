5c34d2e8bbbf00974af210274c35dc01ca2af27e
Jed <jedtony.jt@gmail.com>
2023-10-05 00:46:46 +0100

    Add fail safe for local storage

--- src/helpers/localStorage.ts
+++ src/helpers/localStorage.ts
@@ -1,25 +1,40 @@
 const keyWord: string = "PREACHING_CAMPAIGN_ADMIN";
 
 export const getToken = () => {
+  if (typeof window !== "undefined") {
   return localStorage.getItem(keyWord + "_TOKEN");
+  }
+  return '';
 };
 
 export const setToken = (token: string) => {
+  if (typeof window !== "undefined") {
   localStorage.setItem(keyWord + "_TOKEN", token);
+  }
 };
 
 export const getRole = () => {
+  if (typeof window !== "undefined") {
   return localStorage.getItem(keyWord + "_ROLE");
+}
+return '';
 };
 
 export const setRole = (token: string) => {
+  if (typeof window !== "undefined") {
   localStorage.setItem(keyWord + "_ROLE", token);
+  }
 };
 
 export const getCongregation = () => {
+  if (typeof window !== "undefined") {
   return localStorage.getItem(keyWord + "_CONGREGATION");
+}
+return '';
 };
 
 export const setCongregation = (token: string) => {
+  if (typeof window !== "undefined") {
   localStorage.setItem(keyWord + "_CONGREGATION", token);
+  }
 };
